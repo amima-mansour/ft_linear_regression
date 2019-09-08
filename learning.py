3fdc17a84e1fbbd9e2cfbc92ea8da6b1bbc502f4
amansour <amansour@MacBook-Air-de-amansour.local>
2019-09-08 21:29:17 +0200

    fix pb of linearisation

--- learning.py
+++ learning.py
@@ -2,30 +2,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-import math
 
 
-def init_plot():
+def display_plot(data, x, y, cost_history):
     # Configurate the plot
+    fig, axs = plt.subplots(2, figsize=(8,8))
     # Say, "the default sans-serif font is COMIC SANS"
     plt.rcParams['font.sans-serif'] = "Comic Sans MS"
     # Then, "ALWAYS use sans-serif fonts"
     plt.rcParams['font.family'] = "sans-serif"
-    # plt.title("Price of cars based on their metrage", fontsize = 20, y=1.02, color = 'green', fontweight = 'bold')
-    # plt.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
-    # plt.xlabel("Km", fontsize=14, fontweight = 'bold')
-    # plt.ylabel("Price", fontsize=14, fontweight = 'bold')
+    axs[0].scatter(data[:, 0], data[:, 1], color='blue', label="data")
+    axs[0].plot(x, y, '-r', label="line")
+    axs[0].set_title("Price of cars based on their metrage", fontsize = 15, y=1.02, color = 'green', fontweight = 'bold')
+    axs[0].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[0].set_xlabel("Km", fontsize=14, fontweight = 'bold')
+    axs[0].set_ylabel("Price", fontsize=14, fontweight = 'bold')
+    axs[1].plot(np.arange(10) * 10, cost_history, '-r', label="cost")
+    axs[1].set_title("Cost per iteration", fontsize=15, y=1.02, color='green', fontweight='bold')
+    axs[1].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[1].set_xlabel("Iteration", fontsize=14, fontweight='bold')
+    axs[1].set_ylabel("Cost", fontsize=14, fontweight='bold')
+    plt.tight_layout()
+    plt.show()
 
-def cost_function(data, theta0, theta1):
-    cost = 0.0
-    size = np.shape(data)[0]
-    for el in data:
-        y = el[0]
-        x = el[1]
-        cost = cost + (theta0 + x * theta1 - y) ** 2
+def cost_function(theta, x, y, size):
+    cost = (theta[0] + x * theta[1] - y) ** 2
     cost /= 2 * size
-    return (cost)
+    return(np.sum(cost))
 
 def scale_data(x):
 	return (x - np.mean(x)) / np.std(x)
@@ -36,56 +39,36 @@ def descale_data(x, x_ref):
 def predict(x, theta):
     return theta[0] + theta[1] * x
 
-def estimate_price(theta_0, theta_1, x):
-	return theta_0 + theta_1 * x
-
-def compute_gradients(x, y, m, alpha, iterations):
-	theta = np.zeros((1, 2))
-	for i in range(0, iterations):
-		tmp_theta = np.zeros((1, 2))
-		for j in range(0, m):
-			tmp_theta[0, 0] += (estimate_price(theta[0, 0], theta[0, 1], x[j]) - y[j])
-			tmp_theta[0, 1] += ((estimate_price(theta[0, 0], theta[0, 1], x[j]) - y[j]) * x[j])
-		theta -= (tmp_theta * alpha) / m
-	return theta
-
 def learning(x, y, size, alpha, iterations):
     theta = np.zeros(2)
+    cost_history = []
     for i in range(0, iterations):
         tmp_theta = np.zeros(2)
         for j in range(0, size):
             tmp_theta[0] += predict(x[j], theta) - y[j]
             tmp_theta[1] += ((predict(x[j], theta) - y[j]) * x[j])
-            print(tmp_theta[0])
         theta[0] -= (tmp_theta[0] * alpha) / size
-        theta[1] = (tmp_theta[1] * alpha) / size
-        # print(theta[0], theta[1])
-    return theta
+        theta[1] -= (tmp_theta[1] * alpha) / size
+        if i % 10 == 0:
+            cost_history.append(cost_function(theta, x, y, size))
+    return theta, cost_history
 
 if __name__ == '__main__':
     # Read data from file 'filename.csv'
     data = pd.read_csv("data.csv")
-    # plot scatter
-    init_plot()
-    data.plot(x='km', y='price', label='Price Per Km', kind='scatter', color='blue')
     # convert dataframe to matrix
     data = np.array(data)
     size = np.shape(data)[0]
     # # plt.scatter(data[:, 0], data[:, 1], color='blue')
     # Learning
-    iterations = 200
-    learning_rate = 0.3  # a checker
+    iterations = 100
+    learning_rate = 0.1
     x = scale_data(data[:, 0])
     y = scale_data(data[:, 1])
-    theta =  compute_gradients(x, y, size, learning_rate, iterations)
-    theta[0, 0] = -9.39935521e-18
-    print(theta)
-
+    theta, cost_history =  learning(x, y, size, learning_rate, iterations)
     # use data
-    y = estimate_price(theta[0,0],theta[0,1], x)
+    y = predict(x, theta)
     x = descale_data(x, data[:, 0])
-    y = descale_data(x, data[:, 1])
-    print(y)
-    # Plot Data frame
-    plt.plot(x, y , '-r')
-    # plt.show()
+    y = descale_data(y, data[:, 1])
+    # Plot
+    display_plot(data, x, y, cost_history)
