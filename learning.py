6935192ab7c9a68c0a2bb14c95c83a305becca97
Amina MANSOUR <amansour@e1r1p8.42.fr>
2019-09-09 09:11:38 +0200

    all is ok

--- learning.py
+++ learning.py
@@ -2,28 +2,59 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import metrics
 
-
-def display_plot(data, x, y, cost_history):
+def display_plot(data, x, y, cost_history, theta0_history, theta1_history):
     # Configurate the plot
-    fig, axs = plt.subplots(2, figsize=(8,8))
+    fig, axs = plt.subplots(2, 2, figsize=(8,8))
     # Say, "the default sans-serif font is COMIC SANS"
     plt.rcParams['font.sans-serif'] = "Comic Sans MS"
     # Then, "ALWAYS use sans-serif fonts"
     plt.rcParams['font.family'] = "sans-serif"
-    axs[0].scatter(data[:, 0], data[:, 1], color='blue', label="data")
-    axs[0].plot(x, y, '-r', label="line")
-    axs[0].set_title("Price of cars based on their metrage", fontsize = 15, y=1.02, color = 'green', fontweight = 'bold')
-    axs[0].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
-    axs[0].set_xlabel("Km", fontsize=14, fontweight = 'bold')
-    axs[0].set_ylabel("Price", fontsize=14, fontweight = 'bold')
-    axs[1].plot(np.arange(10) * 10, cost_history, '-r', label="cost")
-    axs[1].set_title("Cost per iteration", fontsize=15, y=1.02, color='green', fontweight='bold')
-    axs[1].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
-    axs[1].set_xlabel("Iteration", fontsize=14, fontweight='bold')
-    axs[1].set_ylabel("Cost", fontsize=14, fontweight='bold')
+    axs[0, 0].scatter(data[:, 0], data[:, 1], color='blue', label="data")
+    axs[0, 0].plot(x, y, '-r', label="line")
+    axs[0, 0].set_title("Price of cars based on their metrage", fontsize = 15, y=1.02, color = 'green', fontweight = 'bold')
+    axs[0, 0].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[0, 0].set_xlabel("Km", fontsize=14, fontweight = 'bold')
+    axs[0, 0].set_ylabel("Price", fontsize=14, fontweight = 'bold')
+    # plot cost
+    axs[0, 1].plot(np.arange(10) * 10, cost_history, '-r', label="cost")
+    axs[0, 1].set_title("Cost per iteration", fontsize=15, y=1.02, color='green', fontweight='bold')
+    axs[0, 1].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[0, 1].set_xlabel("Iteration", fontsize=14, fontweight='bold')
+    axs[0, 1].set_ylabel("Cost", fontsize=14, fontweight='bold')
+    # plot theta0 history
+    axs[1, 0].plot(np.arange(10) * 10, theta0_history, '-r', label="Y-intercept")
+    axs[1, 0].set_title("Y-intercept per iteration", fontsize=15, y=1.02, color='green', fontweight='bold')
+    axs[1, 0].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[1, 0].set_xlabel("Iteration", fontsize=14, fontweight='bold')
+    axs[1, 0].set_ylabel("Y-intercept", fontsize=14, fontweight='bold')
+    # plot theta0 history
+    axs[1, 1].plot(np.arange(10) * 10, theta1_history, '-r', label="Slope")
+    axs[1, 1].set_title("Slope per iteration", fontsize=15, y=1.02, color='green', fontweight='bold')
+    axs[1, 1].legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+    axs[1, 1].set_xlabel("Iteration", fontsize=14, fontweight='bold')
+    axs[1, 1].set_ylabel("Slope", fontsize=14, fontweight='bold')
+    plt.tight_layout()
     plt.tight_layout()
     plt.show()
+
+def rmse(size, y_pred, y_actual):
+    # mean squared error
+    mse = np.sum((y_pred - y_actual) ** 2)
+    # root mean squared error
+    # size is the number of training examples
+    return(np.sqrt(mse / size))
+
+def determintion_coeff(y_pred, y_actual):
+    # sum of square of residuals
+    ssr = np.sum((y_pred - y_actual) ** 2)
+
+    #  total sum of squares
+    sst = np.sum((y_actual - np.mean(y_actual)) ** 2)
+
+    # R2 score
+    return(1 - (ssr / sst))
 
 def cost_function(theta, x, y, size):
     cost = (theta[0] + x * theta[1] - y) ** 2
@@ -41,7 +72,7 @@ def predict(x, theta):
 
 def learning(x, y, size, alpha, iterations):
     theta = np.zeros(2)
-    cost_history = []
+    cost_history, theta0_history, theta1_history = [], [], []
     for i in range(0, iterations):
         tmp_theta = np.zeros(2)
         for j in range(0, size):
@@ -51,7 +82,9 @@ def learning(x, y, size, alpha, iterations):
         theta[1] -= (tmp_theta[1] * alpha) / size
         if i % 10 == 0:
             cost_history.append(cost_function(theta, x, y, size))
-    return theta, cost_history
+            theta0_history.append(theta[0])
+            theta1_history.append(theta[1])
+    return theta, cost_history, theta0_history, theta1_history
 
 if __name__ == '__main__':
     # Read data from file 'filename.csv'
@@ -62,13 +95,22 @@ if __name__ == '__main__':
     # # plt.scatter(data[:, 0], data[:, 1], color='blue')
     # Learning
     iterations = 100
-    learning_rate = 0.1
+    learning_rate = 0.3
     x = scale_data(data[:, 0])
     y = scale_data(data[:, 1])
-    theta, cost_history =  learning(x, y, size, learning_rate, iterations)
+    theta, cost_history, theta0_history, theta1_history =  learning(x, y, size, learning_rate, iterations)
     # use data
     y = predict(x, theta)
     x = descale_data(x, data[:, 0])
     y = descale_data(y, data[:, 1])
+    # performance
+    rmse = rmse(size, y, data[:, 1])
+    print("RMSE PERFORMANCE = {:.3f}".format(rmse))
+    print("R^2 = {:.3f}".format(determintion_coeff(data[:, 1], y)))
     # Plot
-    display_plot(data, x, y, cost_history)
+    display_plot(data, x, y, cost_history, theta0_history, theta1_history)
+    # save data
+    m = (y[0] - y[1]) / (x[0] - x[1])
+    b = m * x[0] * -1 + y[0]
+    theta = [b, m]
+    np.savetxt("theta.txt", theta)
--- predicting.py
+++ predicting.py
@@ -1,25 +1,25 @@
 #!/Users/amansour/.brew/Cellar/python/3.7.4/bin/python3.7
 import predicting as predict
-import os
+import pandas as pd
+import numpy as np
 
 def predict_function(theta0, theta1):
     mileage = input("Please enter a mileage: ")
     try:
         mileage = float(mileage)
-        print("Estimated price for {}Km is {}".format(mileage, theta0 + theta1 * mileage))
+        print(theta0 + theta1 * mileage)
+        print("Estimated price for {} Km is {:.3f}".format(mileage, theta0 + theta1 * mileage))
     except ValueError as error:
         print(error)
         print("error: Please enter a number.")
         predict_function(theta0, theta1)
 
 if __name__ == '__main__':
-    theta0, theta1 = 0, 0
+    theta = [0, 0]
     try:
-        with open('theta.txt', 'r') as f:
-            theta = f.read()
-            theta = theta.split('\n')
-            theta0 = float(theta[0])
-            theta1 = float(theta[1])
+        # Read data from file 'filename.csv'
+        theta = np.loadtxt("theta.txt", dtype=np.longdouble)
+        print(theta)
     except:
         print("Learning is not done yet")
-    predict_function(theta0, theta1)
+    predict_function(theta[0], theta[1])
